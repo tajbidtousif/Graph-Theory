89237df0069729aad7e4a7d7910e4771ec7e0451
Shah Tajbid Tousif <tajbidtousif@gmail.com>
2022-03-17 23:48:59 +0600

    simple example

---
+++ NestedVectors.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printVec(vector<pair<int,int>> &v)
+{
+    cout << "Size: " << v.size() << endl;
+
+    for(auto V:v)
+    {
+        cout << V.first << " " << V.second << endl;
+    }
+}
+
+int main()
+{
+    vector<pair<int, int>> v ;
+
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        v.push_back(make_pair(x, y));
+    }
+
+    printVec(v);
+
+    
+
+}
