d1fb725bdc6a4c6a9d61c5a97f841a6a4e33df82
Shah Tajbid Tousif <tajbidtousif@gmail.com>
2022-03-27 02:10:29 +0600

    DFS Basic Structure

--- DFSBasicStructure.cpp
+++ DFSBasicStructure.cpp
@@ -23,11 +23,12 @@ int main()
 {
     int n, m;
     cin >> n >> m;
+    
     for(int i=0; i<m; i++)
     {
         int v1,v2;
         cin >> v1 >> v2;
-        
+
         graph[v1].push_back(v2);
         graph[v2].push_back(v1);
         
